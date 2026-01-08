e0c085cc73650de838233c3c8563b2ac759b1533
Jaedex designs <96151013+JAEDEXD3S1GN2@users.noreply.github.com>
2026-01-08 14:00:27 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
-
+import WWE from '../../components/WhyweExist/WWE'
+import Themes from '../../components/Whattogain/WhatTG'
 
 const Home = () => {
   return (
     <>
         <Hero />
+        <WWE />
+        <Themes />
     </>
   )
 }
