6e991fb76aafc1bf224fa0552719595567c4626c
Jaedex designs <96151013+JAEDEXD3S1GN2@users.noreply.github.com>
2026-01-09 12:45:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import Home from './pages/Home/Home'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
-
+import Footer from './components/Footer/Footer'
+import Aboutus from './pages/AboutUs/Aboutus'
+import Contactus from './pages/ContactUs/Contactus'
+import Login from './pages/Login/Login'
+import Register from './pages/Signup/Signup'
+import NewsletterSignup from './pages/Newsletter/Newsletter'
+import Blog from './pages/Blog/Blog'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +21,14 @@ function App() {
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<Aboutus />} />
+      <Route path="/Contact" element={<Contactus />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Newsletter" element={<NewsletterSignup />} />
+      <Route path="/Blog" element={<Blog />} />
     </Routes>
+    <Footer />
     </>
   )
 }
