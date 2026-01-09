3c84ff0ffa3a59e11c9c9063d82aed8adfd54929
Jaedex designs <96151013+JAEDEXD3S1GN2@users.noreply.github.com>
2026-01-09 12:35:11 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,12 +25,12 @@ const Navbar = () => {
     <div className="sm:flex sm:justify-between sm:items-center sm:border-b sm:py-3.5 sm:px-2 sm:border-black hidden">
          <div><img className="w-20 h-15" src={Logo} alt="Doabalogo" /></div>
          <div className='text-orangeBrand flex gap-3 font-semibold text-xl font-robotocondensed'>
-         <Link className='hover:text-black'>Home</Link> 
-         <Link className='hover:text-black'>Blog</Link> 
-         <Link className='hover:text-black'>Contact Us</Link> 
-         <Link className='hover:text-black'>About</Link></div>
+         <Link to={"/"} className='hover:text-black'>Home</Link> 
+         <Link to={"/Blog"} className='hover:text-black'>Blog</Link> 
+         <Link to={"/Contact"} className='hover:text-black'>Contact Us</Link> 
+         <Link to={"/about"} className='hover:text-black'>About</Link></div>
          <div className='flex gap-3 text-orangeBrand'><Link className='text-2xl hover:text-black'><FaUser /></Link>
-         <Link className='text-2xl hover:text-black'><FaNewspaper /></Link>
+         <Link to={"/"} className='text-2xl hover:text-black'><FaNewspaper /></Link>
          </div>
     </div>
     <div className='sm:hidden flex justify-between border-b py-3.5 px-2 border-black'>
@@ -38,12 +38,12 @@ const Navbar = () => {
       <div>{mobileNav?  (   
     <div className='flex flex-col gap-3'><div className='text-orangeBrand text-4xl hover:text-black self-end' onClick={() => {closeNav()}} ><IoClose /> </div> 
       <div className='text-orangeBrand flex flex-col gap-3 font-semibold text-xl font-robotocondensed'>
-         <Link className='hover:text-black'>Home</Link> 
-         <Link className='hover:text-black'>Blog</Link> 
-         <Link className='hover:text-black'>Contact Us</Link> 
-         <Link className='hover:text-black'>About</Link></div>
+         <Link to={"/"} className='hover:text-black'>Home</Link> 
+         <Link to={"/Blog"} className='hover:text-black'>Blog</Link> 
+         <Link to={"/Contact"} className='hover:text-black'>Contact Us</Link> 
+         <Link to={"/about"} className='hover:text-black'>About</Link></div>
          <div className='flex gap-3 text-orangeBrand'><Link className='text-2xl hover:text-black'><FaUser /></Link>
-         <Link className='text-2xl hover:text-black'><FaNewspaper /></Link>
+         <Link to={"/"} className='text-2xl hover:text-black'><FaNewspaper /></Link>
          </div>
       </div>  
     ) : (
