8cdef50efc54dbba595455efbf14f3aedc6a2b78
Jaedex designs <96151013+JAEDEXD3S1GN2@users.noreply.github.com>
2026-01-09 12:36:45 +0100

    Add backdrop blur effect to Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div className='bg-center bg-cover bg-Herologo py-60 pl-10'
+    <div className='relative bg-center bg-cover bg-Herologo py-60 pl-10'
     // style={{ backgroundImage: `url(${DiaryBg})` }}
     >
-     <div className='rounded-[10px] bg-cream/50 w-[90%]  sm:w-max px-3 py-5'>
+      <div className='absolute inset-0 bg-black/30 backdrop-blur-sm'></div>
+     <div className=' relative z-10 rounded-[10px] bg-cream/70 w-[90%] sm:w-max px-3 py-5'>
       <h1 className='text-orangeBrand font-bold text-4xl font-robotomono mb-0.5'>The Diary of A Becoming Adult</h1>
-      <p className='text-black font-bold text-xl font-robotocondensed mb-8.75'>Growing up isn't a destination - its's a story still being written..</p>
+      <p className='text-black font-bold text-xl font-robotocondensed mb-8.75'>Growing up isn't a destination - it's a story still being written..</p>
       <Link className='text-black font-bold text-xl font-robotocondensed bg-orangeBrand w-max rounded-md px-3 py-3 hover:bg-green-400 hover:text-white'>Explore Thoughts</Link>
      </div>
     </div>
