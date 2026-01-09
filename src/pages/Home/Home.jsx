29de2929abd0fd5e41c2a2c73f6f9a086dc457af
Jaedex designs <96151013+JAEDEXD3S1GN2@users.noreply.github.com>
2026-01-09 12:38:40 +0100

    Refactor Home component to include new sections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import WWE from '../../components/WhyweExist/WWE'
 import Themes from '../../components/Whattogain/WhatTG'
+import HowItWorks from '../../components/howweoperate/HWO'
+import CTA from '../../components/CTA/CTA'
+
 
 const Home = () => {
   return (
@@ -9,6 +12,8 @@ const Home = () => {
         <Hero />
         <WWE />
         <Themes />
+        <HowItWorks />
+        <CTA />
     </>
   )
 }
