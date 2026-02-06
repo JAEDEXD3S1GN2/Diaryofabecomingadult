9f5e7371c1ac7da2f02708fd8b4284d1b39bbcbf
Jaedex designs <96151013+JAEDEXD3S1GN2@users.noreply.github.com>
2026-02-06 17:59:52 +0100

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     >
       <div className='absolute inset-0 bg-black/30 backdrop-blur-sm'></div>
      <div className=' relative z-10 rounded-[10px] bg-cream/70 w-[90%] sm:w-max px-3 py-5'>
-      <h1 className='text-orangeBrand font-bold text-4xl font-robotomono mb-0.5'>The Diary of A Becoming Adult</h1>
+      <h1 className='text-orangeBrand font-bold text-4xl font-Inter mb-0.5'>The Diary of A Becoming Adult</h1>
       <p className='text-black font-bold text-xl font-robotocondensed mb-8.75'>Growing up isn't a destination - it's a story still being written..</p>
       <Link className='text-black font-bold text-xl font-robotocondensed bg-orangeBrand w-max rounded-md px-3 py-3 hover:bg-green-400 hover:text-white'>Explore Thoughts</Link>
      </div>
@@ -18,3 +18,4 @@ const Hero = () => {
 }
 
 export default Hero
+
