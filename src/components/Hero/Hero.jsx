9643eb19a084f673e4098c28a13b4305bf063bf7
Jaedex designs <96151013+JAEDEXD3S1GN2@users.noreply.github.com>
2026-02-06 18:30:47 +0100

    Change font from robotocondensed to Merriwether
    Updated font family for paragraph text in Hero component.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,12 +10,11 @@ const Hero = () => {
       <div className='absolute inset-0 bg-black/30 backdrop-blur-sm'></div>
      <div className=' relative z-10 rounded-[10px] bg-cream/70 w-[90%] sm:w-max px-3 py-5'>
       <h1 className='text-orangeBrand font-bold text-4xl font-Inter mb-0.5'>The Diary of A Becoming Adult</h1>
-      <p className='text-black font-bold text-xl font-robotocondensed mb-8.75'>Growing up isn't a destination - it's a story still being written..</p>
-      <Link className='text-black font-bold text-xl font-robotocondensed bg-orangeBrand w-max rounded-md px-3 py-3 hover:bg-green-400 hover:text-white'>Explore Thoughts</Link>
+      <p className='text-black font-bold text-xl font-Merriwether mb-8.75'>Growing up isn't a destination - it's a story still being written..</p>
+      <Link className='text-black font-bold text-xl font-Merriwether bg-orangeBrand w-max rounded-md px-3 py-3 hover:bg-green-400 hover:text-white'>Explore Thoughts</Link>
      </div>
     </div>
   )
 }
 
 export default Hero
-
